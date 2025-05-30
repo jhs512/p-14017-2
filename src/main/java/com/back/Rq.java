102cf4437a181c006bd4f49d8469c6877a7f422c
jhs512 <jangka512@gmail.com>
2025-05-30 11:39:59 +0900

    step 7, b2

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -6,21 +6,16 @@ import java.util.Map;
 
 public class Rq {
     private final String cmd;
+    private final Map<String, String> params;
 
     public Rq(String cmd) {
         this.cmd = cmd;
-    }
 
-    public String getActionName() {
-        return cmd.split("\\?", 2)[0];
-    }
-
-    public String getParam(String name, String defaultValue) {
         String queryString = cmd.split("\\?", 2)[1];
 
         String[] queryStringBits = queryString.split("&");
 
-        Map<String, String> params = new HashMap<>();
+        params = new HashMap<>();
 
         for (String paramStr : queryStringBits) {
             String[] paramStrBits = paramStr.split("=", 2);
@@ -29,7 +24,13 @@ public class Rq {
 
             params.put(paramName, paramValue);
         }
+    }
 
+    public String getActionName() {
+        return cmd.split("\\?", 2)[0];
+    }
+
+    public String getParam(String name, String defaultValue) {
         return params.getOrDefault(name, defaultValue);
     }
 }
