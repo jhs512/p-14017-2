04856f31f2c5d326c8d89aaa80b4668860f5aecc
jhs512 <jangka512@gmail.com>
2025-05-30 11:42:06 +0900

    step 7, b3

--- src/main/java/com/back/Rq.java
+++ src/main/java/com/back/Rq.java
@@ -1,8 +1,9 @@
 package com.back;
 
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Rq {
     private final String cmd;
@@ -10,20 +11,11 @@ public class Rq {
 
     public Rq(String cmd) {
         this.cmd = cmd;
-
         String queryString = cmd.split("\\?", 2)[1];
-
-        String[] queryStringBits = queryString.split("&");
-
-        params = new HashMap<>();
-
-        for (String paramStr : queryStringBits) {
-            String[] paramStrBits = paramStr.split("=", 2);
-            String paramName = paramStrBits[0];
-            String paramValue = paramStrBits[1];
-
-            params.put(paramName, paramValue);
-        }
+        params = Arrays
+                .stream(queryString.split("&"))
+                .map(e -> e.split("=", 2))
+                .collect(Collectors.toMap(e -> e[0], e -> e[1]));
     }
 
     public String getActionName() {
